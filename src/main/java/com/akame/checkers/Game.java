a94543ddd5c52ad8f33b24cff2bf0838a3dbd0d0
Oliwia Dykas <dykas.oliwia@gmail.com>
2020-01-24 12:50:40 +0100

    Added scene generator.

---
+++ src/main/java/com/akame/checkers/Game.java
@@ -0,0 +1,56 @@
+package com.akame.checkers;
+
+import com.akame.checkers.model.Piece;
+import com.akame.checkers.model.Tile;
+import com.akame.checkers.types.PieceType;
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.layout.Pane;
+
+public class Game {
+
+    public static final int TILE_SIZE = 100;
+    public static final int WIDTH = 8;
+    public static final int HEIGHT = 8;
+
+    private Tile[][] board = new Tile[WIDTH][HEIGHT];
+
+    private Group tileGroup = new Group();
+    private Group pieceGroup = new Group();
+
+    protected Parent createContent() {
+        Pane root = new Pane();
+        root.setPrefSize(WIDTH * TILE_SIZE, HEIGHT * TILE_SIZE);
+        root.getChildren().addAll(tileGroup, pieceGroup);
+
+        for (int y = 0; y < HEIGHT; y++) {
+            for (int x = 0; x < WIDTH; x++) {
+                Tile tile = new Tile((x + y) % 2 == 0, x, y);
+                board[x][y] = tile;
+
+                tileGroup.getChildren().add(tile);
+
+                Piece piece = null;
+
+                if (y <= 2 && (x + y) % 2 != 0) {
+                    piece = makePiece(PieceType.RED, x, y);
+                }
+
+                if (y >= 5 && (x + y) % 2 != 0) {
+                    piece = makePiece(PieceType.WHITE, x, y);
+                }
+
+                if (piece != null) {
+                    tile.setPiece(piece);
+                    pieceGroup.getChildren().add(piece);
+                }
+            }
+        }
+
+        return root;
+    }
+
+    private Piece makePiece(PieceType type, int x, int y) {
+        return new Piece(type, x, y);
+    }
+}
